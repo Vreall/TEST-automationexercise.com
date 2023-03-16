4c5f2fb865a428af8582da5027423c592654d33b
Vreall <vreal.foto@gmail.com>
2023-03-16 08:53:44 +0100

    Place Order: Register while Checkout -done

--- cypress/integration/automationexercise/add_products_cart.js
+++ cypress/integration/automationexercise/add_products_cart.js
@@ -41,3 +41,4 @@ describe('automationexercise test', function () {
 		);
 	});
 });
+
--- cypress/integration/automationexercise/place_order_register_while_checkout.js
+++ cypress/integration/automationexercise/place_order_register_while_checkout.js
@@ -73,14 +73,56 @@ describe('automationexercise test', function () {
 		cy.contains('a', 'Proceed To Checkout').click();
 		// Verify Address Details and Review Your Order
 		cy.get('ul#address_delivery > li.address_firstname').should(
-			'include',
+			'include.text',
 			'Marek Kowalski'
 		);
+		cy.get('ul#address_delivery > li.address_address1 ')
+			.eq(0)
+			.should('include.text', 'It factory');
+		cy.get('ul#address_delivery > li.address_address1 ')
+			.eq(1)
+			.should('include.text', 'Long Street 20c/10');
+		cy.get('ul#address_delivery > li.address_city ').should(
+			'include.text',
+			'Miami Florida'
+		);
+		cy.get('ul#address_delivery > li.address_country_name ').should(
+			'include.text',
+			'United States'
+		);
+		cy.get('ul#address_delivery > li.address_phone ').should(
+			'include.text',
+			'123456789'
+		);
+		cy.get('#product-1>td.cart_description>h4').should('have.text', 'Blue Top');
+		cy.get('#product-4>td.cart_description>h4').should(
+			'have.text',
+			'Stylish Dress'
+		);
+		cy.get('#product-1>td.cart_quantity>button').should('have.text', '1');
+		cy.get('#product-4>td.cart_quantity>button').should('have.text', '1');
 		// Enter description in comment text area and click 'Place Order'
+		cy.contains('a', 'Place Order').click();
 		// Enter payment details: Name on Card, Card Number, CVC, Expiration date
+		cy.get('[data-qa="name-on-card"]').type('Credit Card');
+		cy.get('[data-qa="card-number"]').type('8983243255346');
+		cy.get('[data-qa="cvc"]').type('908');
+		cy.get('[data-qa="expiry-month"]').type('02');
+		cy.get('[data-qa="expiry-year"]').type('2024');
 		// Click 'Pay and Confirm Order' button
+		cy.get('[data-qa="pay-button"]').click();
 		// Verify success message 'Your order has been placed successfully!'
+		cy.get('[data-qa="order-placed"] + p').should(
+			'have.text',
+			'Congratulations! Your order has been confirmed!'
+		);
 		// Click 'Delete Account' button
+		cy.contains('a', ' Delete Account').click();
 		// Verify 'ACCOUNT DELETED!' and click 'Continue' button
+		cy.get('[data-qa="account-deleted"]').should(
+			'have.text',
+			'Account Deleted!'
+		);
+		cy.get('[data-qa="continue-button"]').click();
 	});
 });
